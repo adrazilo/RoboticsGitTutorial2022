de516fe856d33bb0ce29f954bcd93e55e4e13efa
Alden Webb <WebbAlde148@GOTVDSB.CA>
2022-12-13 20:19:08 -0500

    Added the name Alden

--- main.java
+++ main.java
@@ -2,6 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("SASUKE TIESMA"); 
+        System.out.println("Hello, World!"); 
+        System.out.println("Alden Webb");
     }
 }
